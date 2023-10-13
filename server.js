334639920dba935f9c263ed988c1b7a6d00ffec3
muramasy <mr.maulanaasyarief@gmail.com>
2023-10-13 15:39:02 +0700

    adjust

--- server.js
+++ server.js
@@ -5,26 +5,38 @@ const { Server } = require('socket.io');
 const { createServer } = require("http"); 
 const server = http.createServer(app); 
 const httpServer = createServer(app); 
+
+// inisialisasi socket.io
 const io = require('socket.io')(server, {   
-    cors: { origin: "*"} });  
-    app.get('/', (req, res) => {     
-        res.sendFile(__dirname + "/index.html");   
-});     
+    cors: { origin: "*"} 
+});  
+
+// halaman awal server yang berisikan form untuk mengirim pesan
+app.get('/', (req, res) => {     
+    res.sendFile(__dirname + "/index.html");   
+});
+
+// koneksi ke socket.io
 io.on('connection',(socket)=>{     
     console.log('Client connected');   
 
+    // log ketika koneksi terputus
     socket.on('disconnect',(socket)=>{
         console.log('Client Disconnected');
     }); 
 
+    // mengirim pesan dengan emit 'message'
     socket.on('message', (msg) => {
         io.emit('message', msg)
     });
 
+    // mengirim pesan dengan emit 'reply'
     socket.on('reply', (msg) => {
         io.emit('reply', msg)
     });
 })  
+
+// menjalankan server
 server.listen(3000, () => {   
     console.log('Server is running'); 
 });
