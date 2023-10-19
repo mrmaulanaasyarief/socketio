691a2e46d26cc7c1573ac86e4ee2e581df22d6cb
muramasy <mr.maulanaasyarief@gmail.com>
2023-10-19 08:23:03 +0700

    add pusher

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const { Server } = require('socket.io');
 const { createServer } = require("http"); 
 const server = http.createServer(app); 
 const httpServer = createServer(app); 
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+    appId: '1690322',
+    key: '54276e277a7264e5bb57',
+    secret: 'b56c89f88e7a42ef76e8',
+    useTLS: 'https',
+    cluster: 'ap1', // if `host` is present, it will override the `cluster` option.
+    host: '', // optional, defaults to api.pusherapp.com
+    port: '443', // optional, defaults to 80 for non-TLS connections and 443 for TLS connections
+});
 
 // inisialisasi socket.io
 const io = require('socket.io')(server, {   
@@ -13,7 +24,8 @@ const io = require('socket.io')(server, {
 
 // halaman awal server yang berisikan form untuk mengirim pesan
 app.get('/', (req, res) => {     
-    res.sendFile(__dirname + "/index.html");   
+    pusher.trigger("messages", "MessageCreated", { message: "hello world of liarszz" })  
+    res.sendFile(__dirname + "/index.html"); 
 });
 
 // koneksi ke socket.io
